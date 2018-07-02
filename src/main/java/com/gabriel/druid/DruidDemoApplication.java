9225f1ed219abeb973ed2a440dae4fdd7dfbfd84
高响 <xiang.gao@changhong.com>
2018-07-02 10:37:50 +0800

    Initial commit

---
+++ src/main/java/com/gabriel/druid/DruidDemoApplication.java
@@ -0,0 +1,12 @@
+package com.gabriel.druid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DruidDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DruidDemoApplication.class, args);
+	}
+}
---
+++ src/main/java/com/gabriel/druid/config/DruidConfig.java
@@ -0,0 +1,68 @@
+package com.gabriel.druid.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
+
+/**
+ * @author Gabriel
+ * @email 996492571@qq.com
+ * @time 2018/6/20/020 18:18
+ * @description Druid数据源配置类及监控配置
+ **/
+@Configuration
+@PropertySource(value = "classpath:application.properties")
+public class DruidConfig {
+
+    public DataSource druidDataSource(){
+//        如果用new的方式创建对象，由于缺少init的配置，所以连不上数据库，而采用Builder的方式构建则可以缺省init的配置
+//        return new DruidDataSource();
+        return DruidDataSourceBuilder.create().build();
+    }
+
+    /**
+     * 注册一个StatViewServlet
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean druidStatViewServlet(){
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean();
+        servletRegistrationBean.setServlet(new StatViewServlet());
+        servletRegistrationBean.addUrlMappings("/druid/*");
+        //添加初始化参数：initParams
+        //白名单：
+        servletRegistrationBean.addInitParameter("allow","127.0.0.1");
+        //IP黑名单 (存在共同时，deny优先于allow) : 如果满足deny的话提示:Sorry, you are not permitted to view this page.
+        servletRegistrationBean.addInitParameter("deny","192.168.1.73");
+        //登录查看信息的账号密码.
+        servletRegistrationBean.addInitParameter("loginUsername","admin");
+        servletRegistrationBean.addInitParameter("loginPassword","123456");
+        //是否能够重置数据.
+        servletRegistrationBean.addInitParameter("resetEnable","false");
+        return servletRegistrationBean;
+    }
+
+    /**
+     * 注册一个：filterRegistrationBean
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean druidStatFilter(){
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        //添加过滤规则.
+        filterRegistrationBean.addUrlPatterns("/*");
+        //添加不需要忽略的格式信息.
+        filterRegistrationBean.addInitParameter("exclusions","*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
+    }
+}
